02ec535c77554a8bca12c4559d21af00035fe8eb
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-15 00:02:32 -0300

    statusBar e assets

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Text, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
@@ -7,7 +7,14 @@ export default function App() {
   });
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024', }}>
+      
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent 
+      />
+      
       {fontsLoaded ? <Text>Hello Word!</Text> : <View />} 
     </View>
   );
