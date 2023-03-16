435603fb136d23ec4dcf95ef75f6d953cdbe7e7c
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-15 23:47:37 -0300

    react navigation - routes publics

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading , ScrollView } from "native-base";
 
 import BackgroundImg from '@assets/background.png';
@@ -6,9 +7,16 @@ import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 export function SignUp(){
+
+    const navigation = useNavigation();
+
+    function handleGoBack(){
+        navigation.goBack();
+    }
+
     return (
         <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-            <VStack flex={1} bg="gray.700" px={10} pb={16}>
+            <VStack flex={1} px={10} pb={16}>
                 <Image source={BackgroundImg} alt="Pessoas treinando" resizeMode="contain" position="absolute" />
                 
                 <Center my={24}>
@@ -38,7 +46,7 @@ export function SignUp(){
                 </Center>
 
            
-                <Button title="Voltar para o login" variant="outline" mt={24} />
+                <Button title="Voltar para o login" variant="outline" mt={24} onPress={handleGoBack} />
    
 
             </VStack> 
