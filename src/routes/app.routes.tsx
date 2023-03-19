6b7fb81c009377aec97b6dabbcaf270d2aae6308
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-19 00:51:02 -0300

    esconder abas com rotas ocultas

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -56,6 +56,7 @@ export function AppRoutes(){
             />
             <Screen name="exercise" 
                     component={Exercise} 
+                    options={{ tabBarButton: () => null }}
             />
         </Navigator>
     );
