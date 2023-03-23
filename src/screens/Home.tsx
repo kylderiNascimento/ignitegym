a243ffd406518a795a9854a604b31f587b059f11
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-22 22:57:46 -0300

    Aba History

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -28,7 +28,7 @@ export function Home(){
                 renderItem={({ item }) => (
                     <Group 
                         name={item}
-                        isActive={groupSelected === item}
+                        isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}
                         onPress={() => setGroupSelected(item)}
                     />
                 )}
@@ -57,7 +57,6 @@ export function Home(){
                         paddingBottom: 20
                     }}
                 />
-
 
             </VStack>
 
