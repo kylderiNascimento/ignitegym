f71b52a5c3c6b0a52f64ff68793ee80667078fd5
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-22 23:17:22 -0300

    Finalizando Aba de History

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -34,11 +34,13 @@ export function History(){
                 px={8}
                 contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
                 ListEmptyComponent={() => (
-                <Text color="gray.100" textAlign="center">
-                    Não há exercícios registrados ainda. {'\n'}
-                    Vamos fazer exercícios hoje?
-                </Text>
+                    <Text color="gray.100" textAlign="center">
+                        Não há exercícios registrados ainda. {'\n'}
+                        Vamos fazer exercícios hoje?
+                    </Text>
                 )}
+                showsVerticalScrollIndicator={false}
+
             />
 
         </VStack>
