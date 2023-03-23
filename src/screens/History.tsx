29b6ec86288785f062e147fce72bb844856f3c0e
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-22 23:00:27 -0300

    Utilizando SectionList para agrupamento

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,13 +1,38 @@
 import { HistoryCard } from '@components/HistoryCard';
 import { ScreenHeader } from '@components/ScreenHeader';
-import { Center, Text, VStack } from 'native-base';
+import { Center, Heading, SectionList, Text, VStack } from 'native-base';
+import { useState } from 'react';
 
 export function History(){
+    const [exercises, setExercises] = useState([
+        {
+          title: '26.08.22',
+          data: ["Puxada frontal", "Remada unilateral"]
+        },
+        {
+          title: '27.08.22',
+          data: ["Puxada frontal"]
+        }
+      ]);
+
     return (
         <VStack flex={1}>
-            <ScreenHeader title='Histórico' />
+            <ScreenHeader title='Histórico de exercícios' />
 
-            <HistoryCard />
+            <SectionList
+                sections={exercises}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                <HistoryCard />
+                )}
+                renderSectionHeader={({ section }) => (
+                <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+                    {section.title}
+                </Heading>
+                )}
+                px={8}
+            />
+
         </VStack>
     );
 }
