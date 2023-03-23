cf23afea90d3042bc2b20f7903c0a4ca161df2b7
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-23 19:04:36 -0300

    Finalizando as interfaces - ajustes

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,76 +1,71 @@
 import { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { AppNavigatorRoutesProps } from '@routes/app.routes';
 import { FlatList, Heading, HStack, Text, VStack } from 'native-base';
 
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
 import { ExerciseCard } from '@components/ExerciseCard';
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
 
-export function Home(){
+export function Home() {
 
-    const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro']);
-    const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terras']);
-    const [groupSelected, setGroupSelected] = useState('Costas');
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro']);
+  const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terras']);
+  const [groupSelected, setGroupSelected] = useState('Costas');
 
-    const navigation = useNavigation<AppNavigatorRoutesProps>();
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
 
-    function handleOpenExerciseDetails() {
-      navigation.navigate('exercise');
-    }
-    
-    return (
-        <VStack flex={1}>
-    
-            <HomeHeader />
+  function handleOpenExerciseDetails() {
+    navigation.navigate('exercise');
+  }
 
-            <FlatList 
-                data={groups}
-                keyExtractor={item => item}                
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                _contentContainerStyle={{ px: 8, }}
-                my={10}
-                maxH={10}
-                renderItem={({ item }) => (
-                    <Group 
-                        name={item}
-                        isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}
-                        onPress={() => setGroupSelected(item)}
-                    />
-                )}
-            />
+  return (
+    <VStack flex={1}>
+      <HomeHeader />
 
-            <VStack flex={1} px={8}>
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group 
+            name={item}
+            isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
 
-                <HStack justifyContent="space-between" mb={5}>
-                    <Heading color="gray.200" fontSize="md">
-                        Exercícios
-                    </Heading>
+      <VStack px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md" fontFamily="heading">
+            Exercícios
+          </Heading>
 
-                    <Text color="gray.200" fontSize="sm">
-                        {exercises.length}
-                    </Text>
-                </HStack>
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
 
-                <FlatList 
-                    data={exercises}
-                    keyExtractor={item => item}
-                    renderItem={({ item }) => (
-                        <ExerciseCard  
-                            onPress={handleOpenExerciseDetails} 
-                        />
-                    )}
-                    showsVerticalScrollIndicator={false}
-                    _contentContainerStyle={{
-                        paddingBottom: 20
-                    }}
-                />
+        <FlatList 
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard onPress={handleOpenExerciseDetails} />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{
+            paddingBottom: 20
+          }}
+        />
 
-            </VStack>
-
-            
-            
-        </VStack>
-    );
+      </VStack>
+    </VStack>
+  );
 }
