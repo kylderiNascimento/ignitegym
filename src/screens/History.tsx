cf51c09501f7b0336fbe4e1a79dbf20dab271f6e
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-22 23:11:18 -0300

    Mensagem de lista vazia

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,7 +1,8 @@
+import { useState } from 'react';
+import { Center, Heading, SectionList, Text, VStack } from 'native-base';
 import { HistoryCard } from '@components/HistoryCard';
 import { ScreenHeader } from '@components/ScreenHeader';
-import { Center, Heading, SectionList, Text, VStack } from 'native-base';
-import { useState } from 'react';
+
 
 export function History(){
     const [exercises, setExercises] = useState([
@@ -31,6 +32,13 @@ export function History(){
                 </Heading>
                 )}
                 px={8}
+                contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+                ListEmptyComponent={() => (
+                <Text color="gray.100" textAlign="center">
+                    Não há exercícios registrados ainda. {'\n'}
+                    Vamos fazer exercícios hoje?
+                </Text>
+                )}
             />
 
         </VStack>
