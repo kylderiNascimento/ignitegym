9b7e9b25095c7ccfc20e004e5261f3ebdf18538e
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-23 22:54:18 -0300

    Acessando Album de fotos

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from 'native-base';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
+import * as ImagePicker from 'expo-image-picker';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
@@ -12,6 +13,10 @@ const PHOTO_SIZE = 33;
 export function Profile(){
 
     const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
+    async function handleUserPhotoSelected(){
+        await ImagePicker.launchImageLibraryAsync();
+    }
 
     return (
         <VStack flex={1}>
@@ -36,7 +41,7 @@ export function Profile(){
                     />
                 }
 
-                <TouchableOpacity>
+                <TouchableOpacity onPress={handleUserPhotoSelected}>
                     <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
                     Alterar Foto
                     </Text>
