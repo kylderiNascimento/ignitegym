02b1c01cd90b0027d92d4b78e9ac5c480ce7683d
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-24 15:51:46 -0300

    Formulários - Validando confirmação da senha

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -19,8 +19,11 @@ type FormDataProps = {
 
 // Validando inputs - Utilizando Schema Validations
 const signUpSchema = yup.object({
-    name: yup.string().required('Informe o nome'),
-    email: yup.string().required('Informe o e-mail').email('E-mail inválido') // o .email() verifica se o email é valido
+    name: yup.string().required('Informe o nome.'),
+    email: yup.string().required('Informe o e-mail.').email('E-mail inválido.'), // o .email() verifica se o email é valido
+    password: yup.string().required('Informe a senha').min(6, 'A senha deve ter pelo menos 6 dígitos.'),
+    password_confirm: yup.string().required('Confirme a senha.')
+        .oneOf([yup.ref('password'), null], 'A confirmação da senha não confere') // comparando com o valor de outro input
 });
 
 export function SignUp(){
@@ -100,6 +103,7 @@ export function SignUp(){
                             secureTextEntry
                             onChangeText={onChange}
                             value={value}
+                            errorMessage={errors.password?.message}
                         />
                         )}
                     />
@@ -115,6 +119,7 @@ export function SignUp(){
                             value={value}
                             onSubmitEditing={handleSubmit(handleSignUp)} // chamar a função do botão quando clicar no enviar do teclado
                             returnKeyType="send"
+                            errorMessage={errors.password_confirm?.message}
                         />
                         )}
                     />
