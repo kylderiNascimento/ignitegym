1772450a5c54edd35fe10093297eab580deb675b
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-25 12:36:11 -0300

    API - Axios Tratando Erros da API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,16 +1,17 @@
 import axios from "axios";
+import { AppError } from "@utils/AppError";
 
 const api = axios.create({
   baseURL: 'http://192.168.18.8:3333'
 });
 
 // Interceptions in ERROR or RESPONSE
-api.interceptors.response.use((response) => {
-    console.log("INTERCEPTOR =>", response);
-    return response;
-}, (error) => {
-    console.log('INTERCEPTOR RESPONSE ERROR =>', error)
-    return Promise.reject(error);
+api.interceptors.response.use((response) => response, (error) => {
+    if(error.response && error.response.data) {
+        return Promise.reject(new AppError(error.response.data.message))
+    } else {
+        return Promise.reject(error)
+    }
 });
   
 export { api };
