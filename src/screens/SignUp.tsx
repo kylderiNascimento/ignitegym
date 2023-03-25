ceb9c6660c251dfcd1942778a9edd566ddfcd08c
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-25 11:44:33 -0300

    API - Utilizando Axios

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -4,6 +4,8 @@ import { useForm, Controller } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
+import { api } from "@services/api";
+
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
@@ -38,17 +40,8 @@ export function SignUp() {
 
   async function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
 
-    const response = await fetch('http://192.168.18.8:3333/users', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ name, email, password })
-      });
-
-      const data = await response.json();
-      console.log(data);
+    const response = await api.post('/users', { name, email, password });
+    console.log(response.data);
 
   }
 
