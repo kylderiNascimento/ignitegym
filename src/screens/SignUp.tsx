07db8f6151337060d2fca6cd277f988eed5a6d10
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-25 00:36:22 -0300

    API - Fetch - Obtendo resposta do back-end

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -45,7 +45,8 @@ export function SignUp() {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({ name, email, password })
-      });
+      }).then(response => response.json())
+      .then(data => console.log(data));
 
   }
 
