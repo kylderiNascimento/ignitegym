61aeaef1f9e54bbe1744c36ae2103a9cd25dc773
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-25 01:19:51 -0300

    API - trocando o THEN pelo Async Await

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -36,17 +36,19 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-    console.log({ name, email, password, password_confirm });
-      fetch('http://192.168.18.8:3333/users', {
+  async function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
+
+    const response = await fetch('http://192.168.18.8:3333/users', {
         method: 'POST',
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({ name, email, password })
-      }).then(response => response.json())
-      .then(data => console.log(data));
+      });
+
+      const data = await response.json();
+      console.log(data);
 
   }
 
