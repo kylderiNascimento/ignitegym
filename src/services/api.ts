12e3999c0006da8d70b61557031964b5ff6da230
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-25 12:15:26 -0300

    API - Axios Interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: 'http://192.168.18.8:3333'
 });
+
+// Interceptions in ERROR or RESPONSE
+api.interceptors.response.use((response) => {
+    console.log("INTERCEPTOR =>", response);
+    return response;
+}, (error) => {
+    console.log('INTERCEPTOR RESPONSE ERROR =>', error)
+    return Promise.reject(error);
+});
+  
+export { api };
