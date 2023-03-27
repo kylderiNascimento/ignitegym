b21c9fd457d6cc0bf4027af822c4404b64fc3461
kylderiNascimento <kylderi.nascimento@hotmail.com>
2023-03-27 12:23:40 -0300

    Consumindo o Back-end - Obtendo o ID do Exercício

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,7 +1,7 @@
 import { TouchableOpacity } from 'react-native';
 import { Box, Heading, HStack, Icon, Image, ScrollView, Text, VStack } from 'native-base';
 import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 import { AppNavigatorRoutesProps } from '@routes/app.routes';
 
@@ -10,10 +10,18 @@ import SeriesSvg from '@assets/series.svg';
 import RepetitionsSvg from '@assets/repetitions.svg';
 import { Button } from '@components/Button';
 
+type RouteParamsProps = {
+  exerciseId: string;
+}
+
 export function Exercise() {
 
   const navigation = useNavigation<AppNavigatorRoutesProps>();
 
+  const route = useRoute();
+
+  const { exerciseId } = route.params as RouteParamsProps;
+  
   function handleGoBack() {
     navigation.goBack();
   }
